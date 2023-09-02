7ca5e43fc25ac685c547bc1c0328c11e68c4ce4b
Peter deHaan <peter@deseloper.com>
2023-09-02 09:31:19 -0700

    FIRST BLOOD

---
+++ src/_data/series.js
@@ -0,0 +1,5 @@
+module.exports = [
+  {name: "cat", description: "this is something about a cat."},
+  {name: "dog", description: "this is a sentence about how dogs are better than cats."},
+  {name: "bird", description: "birds are lousy pets."},
+];
